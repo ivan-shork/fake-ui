c310cdf898c746d7e756253992b5377af6a1e553
ivan <494476749@qq.com>
2021-08-25 19:04:25 +0800

    button组件

--- packages/Button/src/index.vue
+++ packages/Button/src/index.vue
@@ -1,19 +1,70 @@
+<!--
+ * @Author: aven9527
+ * @Date: 2021-08-25 18:39:37
+ * @LastEditors: aven9527
+ * @LastEditTime: 2021-08-25 19:03:17
+ * @Description: file content
+-->
 
 <template>
-  <div class="fake-button-container">
-    Fake-Button
-  </div>
+  <button 
+    class="fake-button-container"
+    :class="[
+      'fake-btn-' + type,
+      
+    ]"
+    @click="handleClick"
+
+  >
+    <slot v-if="$slots.default"></slot>
+    <div v-else class="btn-content">
+      <template v-if="loading">
+        <div class="btn-loading-container">
+          <span>*</span>
+          <span class="btn-loading-text">
+            {{ loadingText }}
+          </span>
+        </div>
+      </template>
+      <span class="btn-text" v-else>
+        {{ btnText }}
+      </span>
+    </div>
+  </button> 
 </template>
 
 <script>
   export default {
-    name: 'FakeButton'
+    name: 'FakeButton',
+    props: {
+      type: {
+        type: String,
+        default: 'default'
+      },
+      loading: {
+        type: Boolean,
+        default: false
+      },
+      loadingText: {
+        type: String,
+        default: 'loading'
+      },
+      btnText: {
+        type: String,
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   .fake-button-container {
     
+    .fake-btn {
+
+      &-default {
+        background: ;  
+      }
+    }
   }
 </style>
     
